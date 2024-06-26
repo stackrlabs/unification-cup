10df0b3e4fed12d13f3b733511060ba8ecf82b63
Dhruv Agarwal <91938348+Dhruv-2003@users.noreply.github.com>
2024-06-26 16:47:40 +0530

    feat: actions API endpoint added (#17)
    * feat: actions endpoint added
    
    * fix: minor change in returnType & SDK updated
    
    * chore: bump to v0.5.1
    
    * fix: added ActionInfo in response for actions as requested
    
    * feat: Match , Player & team info added in actions response
    
    * feat: modified actionInfo to contain FE required format
    
    * fix: payload type to AllowedInputs
    
    * fix: query order changed + resolved requested changes
    
    ---------
    
    Co-authored-by: Aashutosh Rathi <aashutosh@stackrlabs.xyz>

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,8 @@ import { ethers } from "ethers";
 import { schemas } from "./stackr/actions";
 import { stackrConfig } from "../stackr.config";
 import { stfSchemaMap } from ".";
+import { LeagueState } from "./stackr/state";
+import { AllowedInputTypes } from "@stackr/sdk";
 
 /**
  * Sign and submit action as operator
@@ -27,4 +29,56 @@ const signAsOperator = async (schemaName: string, inputs: any) => {
   });
 };
 
-export { signAsOperator };
+type ActionInfo = {
+  matchName?: string;
+  teamName?: string;
+  playerName?: string;
+};
+
+const getActionInfo = (
+  payload: AllowedInputTypes,
+  state: LeagueState
+): ActionInfo | null => {
+  const payloadKeys = Object.keys(payload);
+  const actionInfo: ActionInfo = {};
+
+  payloadKeys.forEach((key) => {
+    if (key === "matchId") {
+      const match = state.matches.find((match) => match.id === payload.matchId);
+      if (!match) {
+        return null;
+      }
+      const teamIds = Object.keys(match?.scores).map((k) => parseInt(k));
+      const matchInfo = {
+        matchId: payload.matchId,
+        team1Name: state.teams.find((team) => team.id === teamIds[0])?.name,
+        team2Name: state.teams.find((team) => team.id === teamIds[1])?.name,
+      };
+      actionInfo.matchName = `#${matchInfo.matchId} ${matchInfo.team1Name} vs ${matchInfo.team2Name}`;
+      return;
+    }
+
+    if (key === "playerId") {
+      const player = state.players.find(
+        (player) => player.id === payload.playerId
+      );
+      if (!player) {
+        return null;
+      }
+      actionInfo.playerName = player.name;
+      return;
+    }
+
+    if (key === "teamId") {
+      const team = state.teams.find((team) => team.id === payload.teamId);
+      if (!team) {
+        return null;
+      }
+      actionInfo.teamName = team.name;
+    }
+  });
+
+  return actionInfo;
+};
+
+export { signAsOperator, getActionInfo };
